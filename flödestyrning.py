79484a3739d440766cf6ef95ae780425f57debfa
IsaWad <isak.wadelius@skola.taby.se>
2023-09-13 09:54:38 +0200

    11/9

--- flödestyrning.py
+++ flödestyrning.py
@@ -19,19 +19,53 @@ def upg_två():
 # 3. Gör ett program som hittar det 1000e primtalet. Använd modulooperatorn (%) för att undersöka delbarhet.
 
 def upg_tre():
-    primtal = []
-    tal = 1
+    prime = [1]
+    #used = [1]
+    num = 0
     while True:
-        for i in range(2, tal):
-            if (tal % i) == 0:
+        print(num)
+        #print(used)
+        #print(range(num))
+        num += 1
+        if len(prime) == 1000:
+            break
+        a = 2
+        for a in range(num):
+            if num % a == 0:
                 continue
             else:
-                primtal.append(tal)
-            tal += 1
-        if len(primtal) == 1000:
-            break
-    print(primtal[-1])
-upg_tre()
+                prime.append(num)
+            a +=1
+#upg_tre()
 
+def generate_primes():
+    """
+    Generate the first 'n' prime numbers.
+    
+    :param n: The number of prime numbers to generate.
+    :return: A list of the first 'n' prime numbers.
+    """
+    primes = []
+    num = 2  # Start with the first prime number, 2.
+    n = 100
+    while len(primes) < n:
+        is_prime = True
+        
+        # Check if 'num' is divisible by any of the previously found primes.
+        for prime in primes:
+            if num % prime == 0:
+                is_prime = False
+                break
 
-            
+        if is_prime:
+            primes.append(num)
+
+        # Move on to the next number.
+        num += 1
+    return primes
+
+# Example usage:
+num_primes_to_generate = 1000  # Change this to generate a different number of primes.
+prime_list = generate_primes(num_primes_to_generate)
+print(prime_list)
+
